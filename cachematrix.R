53d3fafed8349338c0815c65983558e63648156c
powerxtreme <kentuckeytom@gmail.com>
2016-08-14 22:46:18 +0800

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+##The first function 'makeCacheMatrix' takes a matrix as argument and returns a list consisting four functions, in other words, 
+##the first funciton creates a new data type(let's name it as 'makeMatrix') that is a list and each element of the list is a funciton. The second function
+##'cacheSolve' takes a 'makeMatrix' as argument  and returns the inverse of the matrix taken by the first funciton if the inverse has not been cached.
+##'##Otherwise, the second funciton will print "getting cached data" and return the inverse afterwards.
 
-## Write a short comment describing this function
+## Thid funciton creates a new 'matrix' object that is capable of caching its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  
+  get <- function() x
+  
+  setInv <- function(inverse) inv <<- inverse
+  
+  getInv <- function() inv
+  
+  list(set = set, get = get, setInv = setInv, getInv = getInv)
 
 }
 
-
-## Write a short comment describing this function
-
+## This function computes the inverse of the new 'matrix' object created by 
+## makeCacheMatrix above. If the inverse has already been calculated (which meansh the 
+## matrix has not changed), then it will return the inverse from the cache.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInv()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setInv(inv)
+  inv
 }
